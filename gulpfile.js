a7188271a5cb831c73b74bfd2cdb0c73131ede7c
Miguel Angel Prudencio Morales <mprudenciom01@gmail.com>
2024-01-31 17:20:24 -0600

    Integracion de Normalize

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
 const { src, dest, watch } = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
+const plumber = require("gulp-plumber");
 
 function css(done) {
     // Pasos para compilar el archivo de SaSS en JS
     src("src/scss/**/*.scss") // Paso 1: Identificiar el archivo de SaSS
+        .pipe(plumber())
         .pipe(sass()) // Paso 2: Compilar
         .pipe(dest("build/css")); // Paso 3: Almacenar en disco duro
 
