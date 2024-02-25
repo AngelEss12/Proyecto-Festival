03c07a4ee662e3197caaa045d3f5d8b7685997d6
Miguel Angel Prudencio Morales <mprudenciom01@gmail.com>
2024-02-24 22:50:14 -0600

    Fijar la barra de navegacion

--- build/js/app.js
+++ build/js/app.js
@@ -5,6 +5,23 @@ document.addEventListener('DOMContentLoaded', function(){
 function iniciarApp() {
     crearGaleria();
     scrollNav();
+    navegacionFija();
+}
+
+function navegacionFija() {
+    const barra = document.querySelector('.header');
+    const sobreFestival = document.querySelector('.sobre-festival');
+    const body = document.querySelector('body');
+
+    window.addEventListener('scroll', () => {
+        if(sobreFestival.getBoundingClientRect().bottom < 0) {
+            barra.classList.add('fijo');
+            body.classList.add('body-scroll');
+        } else {
+            barra.classList.remove('fijo');
+            body.classList.remove('body-scroll');
+        }
+    })
 }
 
 function scrollNav() {
